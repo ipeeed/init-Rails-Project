d25ed915363f7d6c7b4abb080224d550d1f75688
ipd <la24vue@gmail.com>
2020-11-12 13:10:06 +0000

    add column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
     validates :content, presence: true, length: { maximum: 255 } #validationをしている。誤りデータは model.errors変数 に格納される。model.params の変種か。つまりここでは message.errors に格納されている。
                                                                   #model.errors と複数形で書かれている通り、複数のエラーに対応可能。
+    validates :title, presence: true, length: { maximum: 255 }
 end
